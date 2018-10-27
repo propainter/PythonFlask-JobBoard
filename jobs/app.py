f0c2cd074d24e160ded3bf7a0571bb1b283827fc
B0208120 <b0208120@LTB0208120-Mac.local>
2018-10-27 10:31:37 +0530

    df

--- jobs/app.py
+++ jobs/app.py
@@ -17,13 +17,13 @@ def open_connection():
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
-def execute_sql(sql, values, commit=False, single=False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit ==True:
+    if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fetchone() if single else cursor.fetchAll()
+        results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
     return results
 
