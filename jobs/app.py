52a9d06e0f84ade43e3ab7356d5d02bb7c86efbb
B0208120 <b0208120@LTB0208120-Mac.local>
2018-10-27 10:12:20 +0530

    df

--- jobs/app.py
+++ jobs/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-naukri = Flask(__name__)
+app = Flask(__name__)
 
 IndexTemplate = 'index.html'
 
-@naukri.route('/')
-@naukri.route('/jobs')
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     return render_template(IndexTemplate)
