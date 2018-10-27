f83da7d055a5f7dedfcbefb9a1c0a40fbfc2ac76
B0208120 <b0208120@LTB0208120-Mac.local>
2018-10-27 10:29:44 +0530

    df

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
@@ -8,3 +9,27 @@ IndexTemplate = 'index.html'
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
+
+PATH = 'db/jobs.sqlite'
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection  == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+def execute_sql(sql, values, commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit ==True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchAll()
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(execption):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
